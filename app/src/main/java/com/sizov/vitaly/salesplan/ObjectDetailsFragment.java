588feecd5983cfa125f6a0101ba6dc88076ac5fe
Vitaly Sizov <vitalii_sizov@mail.ru>
2018-02-08 00:00:27 +0600

    The type of diagrams and some of its settings is changed

--- app/src/main/java/com/sizov/vitaly/salesplan/ObjectDetailsFragment.java
+++ app/src/main/java/com/sizov/vitaly/salesplan/ObjectDetailsFragment.java
@@ -11,11 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,7 +36,7 @@ public class ObjectDetailsFragment extends Fragment {
 
     private final static String TAG = "ObjectDetailsFragment";
 
-    private LineChart mChartCurrentSales;
+    private BarChart mChartCurrentSales;
 
     private TextView mNameTextView;
     private TextView mTotalCurrentSalesTextView;
@@ -58,7 +68,7 @@ public class ObjectDetailsFragment extends Fragment {
         mTotalCurrentSalesTextView = (TextView) view.findViewById(R.id.total_current_sales_text_view);
         mTotalCurrentSalesTextView.setText(String.format("%(.2f", mTotalCurrentSales) + "$");
 
-        mChartCurrentSales = (LineChart) view.findViewById(R.id.chart_sales);
+        mChartCurrentSales = (BarChart) view.findViewById(R.id.chart_sales);
 
 
         setSettingChart();
@@ -71,23 +81,48 @@ public class ObjectDetailsFragment extends Fragment {
 
         mChartCurrentSales.setEnabled(true);
         mChartCurrentSales.setScaleEnabled(false);
+        mChartCurrentSales.getDescription().setText("");
 
         mChartCurrentSales.setDrawBorders(true);
         mChartCurrentSales.setBorderColor(Color.GRAY);
 
-        ArrayList<Entry> values = new ArrayList<>();
+        final ArrayList<BarEntry> values = new ArrayList<>();
 
         for (int i = 0; mCurrentSales.length > i; i++) {
-            values.add(new Entry(i, (float) mCurrentSales[i]));
+            values.add(new BarEntry(i, (float) mCurrentSales[i]));
         }
 
-        LineDataSet set = new LineDataSet(values, "Data Set 1");
+        BarDataSet set = new BarDataSet(values, "The current sales on hours");
+        int colorPrimaryTins = Color.parseColor("#6573c3");
+        set.setColor(colorPrimaryTins);
 
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set);
 
-        LineData dataSet = new LineData(dataSets);
-        mChartCurrentSales.setData(dataSet);
+        final ArrayList<String> xLabel = new ArrayList<>();
+        for (int i=1; i <= mCurrentSales.length; i++) {
+            xLabel.add(8 + i + ":00");
+        }
+
+        
+        XAxis xAxis = mChartCurrentSales.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return xLabel.get((int)value);
+            }
+        });
+
+        BarData data = new BarData(set);
+
+        Legend legend = mChartCurrentSales.getLegend();
+        legend.setTextSize(15f);
+        legend.setFormSize(15f);
+
+        data.setValueTextSize(10f);
+
+        mChartCurrentSales.animateXY(2500, 2500);
+        mChartCurrentSales.setData(data);
     }
 }
 
